cd7a552e5074e12a42d662007adf346f37ef1be1
Kasiet Tokbaeva <kasiet.tokbaeva.kk@gmail.com>
2023-12-16 17:31:47 +0600

    31github

---
+++ script.js
@@ -0,0 +1,27 @@
+/*let myString = "Это строка";
+let myString2 = 'Это тоже строка';
+
+let userName = "Mirlan";
+console.log(userName.length);   //показывает из скольки символов состоит строка. Длина строки
+
+let userName = "Mirlan";
+console.log(userName[2]);  */     //доступ к символам по индексам
+
+
+//let userNum = "$120";
+//document.write(userNum.slice(1,4));
+
+
+let userName = prompt("Здравствуй, я компьютер, а тебя как зовут?");
+console.log(userName);
+let userAge = prompt(userName + ", очень приятно. Сколько тебе лет?");
+console.log(userAge);
+let usersay = prompt("Ого! Тебе уже " +userAge);
+
+
+
+
+
+
+
+
